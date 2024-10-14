bc9129de4541249600b1e11a00336c42d684a0cc
AndresVargas1 <93749091+AndresVargas1@users.noreply.github.com>
2024-10-14 17:53:55 -0500

    Modelo proyecto

--- src/ECOFORGE/MODELO/Proyecto.java
+++ src/ECOFORGE/MODELO/Proyecto.java
@@ -6,8 +6,31 @@ package ECOFORGE.MODELO;
 
 /**
  *
- * @author YANFER
+ * @author ANDRES
  */
 public class Proyecto {
+    private Integer codigo_proyecto;
+    private String nombre_proyecto;
+
+    public Proyecto(Integer codigo_proyecto, String nombre_proyecto) {
+        this.codigo_proyecto = codigo_proyecto;
+        this.nombre_proyecto = nombre_proyecto;
+    }
+
+    public Integer getCodigo_proyecto() {
+        return codigo_proyecto;
+    }
+
+    public void setCodigo_proyecto(Integer codigo_proyecto) {
+        this.codigo_proyecto = codigo_proyecto;
+    }
+
+    public String getNombre_proyecto() {
+        return nombre_proyecto;
+    }
+
+    public void setNombre_proyecto(String nombre_proyecto) {
+        this.nombre_proyecto = nombre_proyecto;
+    }
     
 }
