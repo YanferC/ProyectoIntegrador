b284a9ba989d0712acbae66a7ccace54ed7d05d6
Yanfer Castro <yanferavila5075@gmail.com>
2024-10-19 00:13:11 -0500

    Nuevos cambios

--- src/ECOFORGE/VISTA/Asesor.java
+++ src/ECOFORGE/VISTA/Asesor.java
@@ -129,7 +129,7 @@ public class Asesor extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
